ba44d4a48a7ce3b32bc748109a926db3d1a7ba58
Burak Altin <altinburak@gmail.com>
2022-12-17 22:41:15 +1100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+
+namespace GPT_3_Connection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a prompt:");
+            string prompt = Console.ReadLine();
+
+            string apiKey = "Your API Key";
+            string model = "text-davinci-002";
+            int maxTokens = 1024;
+            float temperature = 0.5f;
+
+            // Build the API request
+            string requestUrl = $"https://api.openai.com/v1/completions";
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+
+            var requestJson = new
+            {
+                prompt = prompt,
+                max_tokens = maxTokens,
+                temperature = temperature,
+                model = model
+            };
+
+            StringContent content = new StringContent(JsonConvert.SerializeObject(requestJson), Encoding.UTF8, "application/json");
+
+            // Send the request and receive the response
+            HttpResponseMessage response = client.PostAsync(requestUrl, content).Result;
+            string responseJson = response.Content.ReadAsStringAsync().Result;
+
+            // Extract the completed text from the response
+            dynamic responseObject = JsonConvert.DeserializeObject(responseJson);
+            string completedText = responseObject.choices[0].text;
+
+            Console.WriteLine("Completed text: " + completedText);
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # GPT3ConnectionExample
 GPT-3 Connection Example
+
+You can use this example to connect GPT-3 and read the result.
+
+I leave how to use the result to your imagination.
+
